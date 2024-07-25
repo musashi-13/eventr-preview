c973396b9dd8b57ee0340d467c5d3d07347507bf
Karan Hathwar <karanhathwar@gmail.com>
2024-07-25 18:26:51 +0530

    Signin API, login UI (prototype)

--- eventr-frontend/app/page.tsx
+++ eventr-frontend/app/page.tsx
@@ -6,6 +6,7 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import EventCard from "./components/eventCard";
 import LogIn from "./components/logIn";
+import SignIn from "./components/SignIn";
 config.autoAddCss = false
 
 
@@ -14,7 +15,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between">
       <NavBar pageId={1}/>
       {/* <EventCard eventId="lorem ipsum"/> */}
-      <LogIn/>
+      <SignIn/>
+      
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
